0714b1c247e3dec872b34513236c99dae723780a
patrickdurke <50921587+patrickdurke@users.noreply.github.com>
2020-11-18 13:33:20 +0100

    ConnectionAddress property i Company.cs

--- ForagerWebAPIDB/Models/Company.cs
+++ ForagerWebAPIDB/Models/Company.cs
@@ -7,12 +7,15 @@ namespace ForagerWebAPIDB.Models
     {
         [Key]
         public int CompanyId { get; set; }
-        public string Cvr { get; set; }
+        public string Cvr { get; set; } // Udfyld
         public double TrustScore { get; set; }
         public int NumberOfVotes { get; set; }
-        public string Name { get; set; }
-        public string Location { get; set; }
+        public string Name { get; set; } // Udfyld
+        public string Address { get; set; } // Udfyld
+        public string Postcode { get; set; } // Udfyld
         public string Logo { get; set; }
         public List<Employee> Employees { get; set; }
+        public string ConnectionAddress { get; set; } //Udfyld
+
     }
 }
